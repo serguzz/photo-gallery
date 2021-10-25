9e43552c27b592d711dbae33a79f9468da4b0a89
Sergiy <serguz@ukr.net>
2021-10-25 14:18:25 +0300

    Navbar created in a separate file: Navbar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,35 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <Navbar/>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+    <v-card class="overflow-hidden">
+      <v-sheet
+        id="scrolling-techniques-3"
+        class="overflow-y-auto"
+        max-height="600"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+        <v-container style="height: 1000px;">
+
+        </v-container>
+
+      </v-sheet>
+
+    </v-card>
 
     <v-main>
-      <HelloWorld/>
+  
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
+import Navbar from "@/components/Navbar";
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Navbar
   },
 
   data: () => ({
---
+++ src/components/Navbar.vue
@@ -0,0 +1,67 @@
+<template>
+      <v-app-bar
+        absolute
+        color="#6A76AB"
+        dark
+        shrink-on-scroll
+        prominent
+        src="https://picsum.photos/1920/1080?random"
+        fade-img-on-scroll
+        scroll-target="#scrolling-techniques-3"
+      >
+        <template v-slot:img="{ props }">
+          <v-img
+            v-bind="props"
+            gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+          ></v-img>
+        </template>
+  
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  
+        <v-app-bar-title>Photo Gallery</v-app-bar-title>
+  
+        <v-spacer></v-spacer>
+  
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+  
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+  
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+  
+        <template v-slot:extension>
+          <v-tabs align-with-title>
+            <v-tab>Home</v-tab>
+            <v-tab>Gallery</v-tab>
+            <v-tab>Contact</v-tab>
+
+            <v-spacer/>
+            <v-btn class="orange mr-1">Home</v-btn>
+            <v-btn class="orange mr-1">Callery</v-btn>
+
+          </v-tabs>
+        </template>
+
+
+      </v-app-bar>
+    
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
+
+<style scoped>
+
+</style>
